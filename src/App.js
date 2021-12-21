bae0b7c0d251f570fc0ea7c18b88f709b1110955
r14minji <rkdalswl0728@gmail.com>
2021-12-21 10:49:52 +0900

    step2 section 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import './css/style.css';
+import Footer from './components/Footer.js';
+import {Logo, Menu} from './components/Header.js';
+
+const name1 = "David"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="btns">
-          <li>button1</li>
-          <li>button2</li>
-        </div>
-      </header>
+      {/* 전체 레이아웃 */}
+      <figure>
+        <Logo />
+        <Menu/>
+
+          <section>
+            <article>
+              <div className="inner">
+                {name1}
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Andy
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Michael
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Julia
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Christoper
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Emma
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Catherin
+              </div>
+            </article>
+            <article>
+              <div className="inner">
+                Alice
+              </div>
+            </article>
+          </section>
+        <Footer />
+
+
+      </figure>
     </div>
   );
 }

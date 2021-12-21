26210dcee484746e46a7efd77dbaa905c3e7e9a3
r14minji <rkdalswl0728@gmail.com>
2021-12-21 17:27:26 +0900

    기본 컨텐츠 h1, menu, footer 작성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './css/style.css';
-import Footer from './components/Footer.js';
-import Panel from './components/Panel';
-import {Logo, Menu} from './components/Header.js';
 
 
 
 
 function App() {
   return (
-    <div className="APP">
-      <ul className="btns">
-        <li>button1</li>
-        <li>button2</li>
-      </ul>
+    <div className="App">
+
+    <figure>
+      <h1>
+        <strong>DECODELAB</strong><br />
+        <span>UI/UX DESIGN&DEVELOPMENT</span>
+      </h1> 
+      <a href="#" className="menu">
+        <i className="fas fa-bars"></i>
+      </a> 
+      <p>2022 DCODELAB &copy; ALL RIGHTS RESERVED.</p>
+    </figure>
+
+
+
     </div>
   );
 }

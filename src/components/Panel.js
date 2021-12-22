a03024f349bf589f4b56ca3c44118e2591feb0af
r14minji <rkdalswl0728@gmail.com>
2021-12-22 11:21:47 +0900

    useState를 활용하여 state변경 전용함ㅁ수로 state값 변경하여 JSX 재 랜더링

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,20 +1,29 @@
+import { useState } from "react";
+
 function Panel() {
 	const arr = ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers'];
 	const path = process.env.PUBLIC_URL;
 	const deg = 360/arr.length;	
 	const btnStyle = {position: 'fixed', top:0, left:0}
 	
-	function changeArr(txt){
-		arr[0] = txt;
-		console.log(arr);
+	let [names, setNames] = useState(arr);
+	//console.log(names); //(8) ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers']
+	//console.log(setNames); // ƒ dispatchAction(fiber, queue, action) {...} 
+
+	const changeState = () =>{
+		let newArr = [...arr]; //새로운 배열로 복사함
+		newArr[0] = 'Escape';
+		console.log(newArr); //(8) ['Escape', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers']
+		console.log(arr); // (8) ['Blizzards', 'Calm', 'Dusty_Road', 'Escape', 'Payday', 'Retreat', 'Seasonal', 'Vespers']
+		setNames(newArr); // 변경된 함수로 값을 변경한다.
 	}
 
 	return (
 		<>
-		<button style={btnStyle} onClick={()=>changeArr('Escape')}>button</button>
+		<button style={btnStyle} onClick={changeState}>button</button>
 		<section>
 		{
-			arr.map((data,index)=>{	
+			names.map((data,index)=>{	
 				let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}	
 				let imgSrc = `${path}/img/${data}.jpg`
 
